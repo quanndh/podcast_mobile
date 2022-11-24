f34438fa8faa7262757028e3700bc5f034fafef0
quanndh1810 <quanndh1810@gmail.com>
2022-11-24 20:41:43 +0700

    .

--- src/components/AppText/index.tsx
+++ src/components/AppText/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, TextProps } from 'react-native';
+import { Colors } from '../../constants/colors';
 
 interface AppTextProps extends TextProps {}
 
@@ -17,6 +18,7 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     lineHeight: 17,
     fontFamily: 'SF Pro Display',
+    color: Colors.black,
   },
 });
 
